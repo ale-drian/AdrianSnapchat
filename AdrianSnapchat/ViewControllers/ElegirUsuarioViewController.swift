4ca127c45c808920c0069f222762753144a36467
ale-drian <thalia.adrian@tecsup.edu.pe>
2021-06-10 04:24:07 -0500

    Avance Tarea (Interfaces)

--- AdrianSnapchat/ViewControllers/ElegirUsuarioViewController.swift
+++ AdrianSnapchat/ViewControllers/ElegirUsuarioViewController.swift
@@ -7,23 +7,51 @@
 //
 
 import UIKit
+import Firebase
 
 class ElegirUsuarioViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var listaUsuarios: UITableView!
     
+    var usuarios:[Usuario] = []
+    var ref = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         listaUsuarios.delegate = self
         listaUsuarios.dataSource = self
+        ref.child("usuario").observe(DataEventType.childAdded, with: { (snapshot) in
+            print(snapshot)
+            //106
+            let usuario = Usuario()
+            usuario.email = (snapshot.value as! NSDictionary)["email"] as! String
+            usuario.uid = snapshot.key
+            self.usuarios.append(usuario)
+            self.listaUsuarios.reloadData()
+            print("cargando")
+        })
     }
     
+    /*102
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        listaUsuarios.delegate = self
+        listaUsuarios.dataSource = self
+        ref.child("usuario").observe(DataEventType.childAdded, with: { (snapshot) in
+            print(snapshot)
+        })
+    }*/
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return 0
+        return usuarios.count
      }
      
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         return UITableViewCell()
+         let cell = UITableViewCell()
+         let usuario = usuarios[indexPath.row]
+         print("\(usuario)")
+         cell.textLabel?.text = usuario.email
+         return cell
      }
 
     /*
